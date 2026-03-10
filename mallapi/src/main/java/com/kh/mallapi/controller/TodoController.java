148c8727f711b13c9322bd48da95fd58e5055515
ohjh9701 <ohjh4875@gmail.com>
2026-03-10 10:39:05 +0900

    [추가] JPA 프로젝트 실습 - 생성, 수정, 삭제 기능 추가

--- mallapi/src/main/java/com/kh/mallapi/controller/TodoController.java
+++ mallapi/src/main/java/com/kh/mallapi/controller/TodoController.java
@@ -1,7 +1,13 @@
 package com.kh.mallapi.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +37,27 @@ public class TodoController {
 	public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO) {
 		log.info(pageRequestDTO);
 		return service.list(pageRequestDTO);
+	}
+
+	@PostMapping("/")
+	public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+		log.info("TodoDTO: " + todoDTO);
+		Long tno = service.register(todoDTO);
+		return Map.of("TNO", tno);
+	}
+
+	@PutMapping("/{tno}")
+	public Map<String, String> modify(@PathVariable(name = "tno") Long tno, @RequestBody TodoDTO todoDTO) {
+		todoDTO.setTno(tno);
+		log.info("Modify: " + todoDTO);
+		service.modify(todoDTO);
+		return Map.of("RESULT", "SUCCESS");
+	}
+
+	@DeleteMapping("/{tno}")
+	public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+		log.info("Remove: " + tno);
+		service.remove(tno);
+		return Map.of("RESULT", "SUCCESS");
 	}
 }
